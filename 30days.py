a345f705de3be3e07fb4d0671582d460af890caa
E Z <ez@Es-MacBook-Pro.local>
2023-02-22 22:55:26 -0800

    Added sliders

--- 30days.py
+++ 30days.py
@@ -40,3 +40,43 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+#Slider
+
+from datetime import time, datetime
+
+st.header('st.slider')
+
+#slider 1
+st.subheader('Slider')
+
+age = st.slider('How old are you ?', 0, 130, 25)
+
+st.write("I'm", age, 'years old')
+
+#slider 2
+st.subheader('Range Slider')
+
+values = st.slider(
+    'Select range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values: ', values)
+
+#slider 3
+
+st.subheader('Range time slider')
+
+appointment =  st.slider(
+    "Schedule your appointment: ",
+    value = (time(11,30), time(12,45)))
+st.write("You're scheduled for: ", appointment)
+
+#slider 4
+
+st.subheader('Date Slider')
+
+start_time = st.slider(
+    "When do you start?",
+    value = datetime(2020, 1, 1, 9, 30),
+    format = 'MM/DD/YY - hh:mm')
+st.write("Start time: ", start_time)
