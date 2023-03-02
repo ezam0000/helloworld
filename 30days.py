24f59e2e19f50d4d9b27a2dfe008685a811c4e89
E Z <ez@EZ-MacBook-Pro-M2-Max.local>
2023-03-02 12:50:42 -0800

    added pandas profiling

--- 30days.py
+++ 30days.py
@@ -138,6 +138,8 @@ if cola:
      st.write("Here you go 🥤")
 
 #Day 13
+import streamlit as st
+import pandas as pd
 import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
