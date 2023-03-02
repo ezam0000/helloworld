8771edbf6c539ece65b793688cb575b2346c8e02
E Z <ez@EZ-MacBook-Pro-M2-Max.local>
2023-03-02 12:30:25 -0800

    added checkbox

--- 30days.py
+++ 30days.py
@@ -118,3 +118,21 @@ options = st.multiselect(
 
 st.write ('You selected', option)
 
+#Day 12 
+
+st.header('Checkbox')
+
+st.write ('What would you like to order?')
+
+icecream = st.checkbox('Ice cream')
+coffee = st.checkbox('Coffee')
+cola = st.checkbox('Cola')
+
+if icecream:
+     st.write("Great! Here's some more 🍦")
+
+if coffee: 
+     st.write("Okay, here's some coffee ☕")
+
+if cola:
+     st.write("Here you go 🥤")
